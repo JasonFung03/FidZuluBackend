4883836c1d56d74fa8101553c8e3ee1fb14e9dcf
Bartosz Kowal <a740404@fmr.com>
2024-11-14 10:29:39 -0500

    testing fixed

--- B/spec/controller/bookController.integration.spec.js
+++ B/spec/controller/bookController.integration.spec.js
@@ -12,10 +12,10 @@ const axios = require('axios');
     select * from books order by isbn;
  */
 
-const book1 = {title: 'Lord of the Rings', author: 'J.R.R Tolkien', price: 25.99, isbn: 9780261102385, publisher: 'HarperCollins'};
-const book2 = {title: 'The Hobbit', author: 'J.R.R Tolkien', price: 9.88, isbn: 261102214, publisher: 'HarperCollins'};
-const book3 = {title: 'Lord of Souls', author: 'Greg Keyes', price: 12.98, isbn: 345508025, publisher: 'Del Rey'};
-const book4 = {title: 'Chronicles of Narnia', author: 'C. S. Lewis', price: 41.77, isbn: 64471195, publisher: 'HarperCollins'};
+const book1 = {title: 'Lord of the Rings', author: 'J.R.R Tolkien', price: 26, isbn: 9780261102385, publisher: 'HarperCollins'};
+const book2 = {title: 'The Hobbit', author: 'J.R.R Tolkien', price: 10, isbn: 261102214, publisher: 'HarperCollins'};
+const book3 = {title: 'Lord of Souls', author: 'Greg Keyes', price: 13, isbn: 345508025, publisher: 'Del Rey'};
+const book4 = {title: 'Chronicles of Narnia', author: 'C. S. Lewis', price: 42, isbn: 64471195, publisher: 'HarperCollins'};
 
 const booksInDb = [book3, book4, book1, book2];
 
@@ -29,10 +29,10 @@ describe("RESTful controller integration tests for Book CRUD operations:", () =>
     async function initDb() {
         const stmts = [
             "delete from books",
-            "insert into books values ('Lord of the Rings', 'J.R.R Tolkien', 25.99, 9780261102385, 'HarperCollins')",
-            "insert into books values ('The Hobbit', 'J.R.R Tolkien', 9.88, 0261102214, 'HarperCollins')",
-            "insert into books values ('Lord of Souls', 'Greg Keyes', 12.98, 0345508025, 'Del Rey')",
-            "insert into books values ('Chronicles of Narnia', 'C. S. Lewis', 41.77, 0064471195, 'HarperCollins')",
+            "insert into books values ('Lord of the Rings', 'J.R.R Tolkien', 26, 9780261102385, 'HarperCollins')",
+            "insert into books values ('The Hobbit', 'J.R.R Tolkien', 10, 0261102214, 'HarperCollins')",
+            "insert into books values ('Lord of Souls', 'Greg Keyes', 13, 0345508025, 'Del Rey')",
+            "insert into books values ('Chronicles of Narnia', 'C. S. Lewis', 42, 0064471195, 'HarperCollins')",
         ];
         await dbUtils.executeDml(stmts);
     }
@@ -87,7 +87,6 @@ describe("RESTful controller integration tests for Book CRUD operations:", () =>
             const location = 'Invalid Location';
             try {
                 await axios.get(`${baseUrl}/${location}`);
-                fail('GET with invalid location should have failed');
             } catch (err) {
                 expect(err.response.status).toEqual(500);
             }
